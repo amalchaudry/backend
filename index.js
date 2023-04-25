8bf0209927a3dfb1b677b3807a44dfe6ab0be2d0
akulkarni334 <akulkarni334@gatech.edu>
2023-04-25 16:41:01 -0400

    Add POST requests for addAirport and grantPilotLicense

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const cors = require("cors");
 const mysql = require("mysql2");
 const app = express();
 
+// This is so that the frontend can make requests to the backend
 app.use(cors());
+
+// This is to parse the inputs passed in the body of POST requests
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -36,8 +42,15 @@ const tables = [
   "route_path",
   "ticket",
   "ticket_seats",
+  "flights_in_the_air",
+  "flights_on_the_ground",
+  "people_in_the_air",
+  "people_on_the_ground",
+  "route_summary",
+  "alternative_airports",
 ];
 
+// Get all tables and views
 app.get("/api/:table", (req, res) => {
   const table = req.params.table;
   if (!tables.includes(table)) {
@@ -47,6 +60,44 @@ app.get("/api/:table", (req, res) => {
   connection.query(query, (err, results, fields) => {
     if (err) {
       console.error("Error fetching data from database: ", err);
+      res.sendStatus(500);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+// Call procedure to add airport
+app.post("/api/addAirport", (req, res) => {
+  const airportID = req.body.airportID;
+  const airport_name = req.body.airport_name;
+  const city = req.body.city;
+  const state = req.body.state;
+  const locationID = req.body.locationID;
+  const query = `CALL add_airport(?, ?, ?, ?, ?)`;
+  connection.query(
+    query,
+    [airportID, airport_name, city, state, locationID],
+    (err, results, fields) => {
+      if (err) {
+        console.error("Error adding airport to database: ", err);
+        res.sendStatus(500);
+      } else {
+        res.send(results);
+      }
+    }
+  );
+});
+
+// Call procedure to grand pilot license
+app.post("/api/grantPilotLicense", (req, res) => {
+  const personID = req.body.personID;
+  const licenseID = req.body.licenseID;
+
+  const query = `CALL grant_pilot_license(?, ?)`;
+  connection.query(query, [personID, licenseID], (err, results, fields) => {
+    if (err) {
+      console.error("Error granting pilot license: ", err);
       res.sendStatus(500);
     } else {
       res.send(results);
